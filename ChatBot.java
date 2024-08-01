f2369d69e4b38db10ecd65e10d61756eeba8757d
igordon215 <igordon215@gmail.com>
2024-08-01 15:45:17 -0400

    start chatbot

---
+++ ChatBot.java
@@ -0,0 +1,5 @@
+public class ChatBot {
+    private static final String STOCK_SPI_URL = "";
+    private static final String SPORTS_API_URL = "";
+    private static final String WEATHER_API_URL = "";
+}
