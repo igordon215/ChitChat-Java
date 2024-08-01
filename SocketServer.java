5c717f6c29e76abca18b9601ef1d0db5a96416d1
igordon215 <igordon215@gmail.com>
2024-08-01 10:28:24 -0400

    added logger

--- SocketServer.java
+++ SocketServer.java
@@ -5,7 +5,10 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.logging.*;
 
 public class SocketServer {
     ServerSocket server;
@@ -14,20 +17,30 @@ public class SocketServer {
 
     ArrayList<ServerThread> list = new ArrayList<ServerThread>();
 
+    // Add Logger
+    static final Logger logger = Logger.getLogger(SocketServer.class.getName());
+
     public SocketServer() {
         try {
+            // Set up Logging
+            setupLogging();
+
             // Set up the server address (localhost)
             addr = InetAddress.getByName("127.0.0.1");
             //addr = InetAddress.getByName("192.168.43.1");
 
             //Create a ServerSocket on port 1234 with a backlog of 50 connections
             server = new ServerSocket(1234, 50, addr);
+            // Adding some logger text
+            logger.info("Server started. Waiting for client connections...");
             System.out.println("\n Waiting for Client connection");
             // Start the client
             SocketClient.main(null);
             while (true) {
                 // Accept incoming client connections
                 sk = server.accept();
+                // Adding connected message
+                logger.info("New connection from: " + sk.getInetAddress());
                 System.out.println(sk.getInetAddress() + " connect");
 
                 //Create and start a new thread for the connected client
@@ -36,18 +49,45 @@ public class SocketServer {
                 st.start();
             }
         } catch (IOException e) {
+            // logger fail message
+            logger.severe("ServerSocket failed: " + e.getMessage());
             System.out.println(e + "-> ServerSocket failed");
         }
     }
 
+    // Setup logging method
+    private void setupLogging() throws IOException {
+        // Create file handler
+        FileHandler fileHandler = new FileHandler("server_log.txt", true);
+
+        // Create formatter
+        SimpleFormatter formatter = new SimpleFormatter() {
+            @Override
+            public synchronized String format(LogRecord lr) {
+                return String.format("[%s] %s: %s%n",
+                        LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                        lr.getLevel().getLocalizedName(),
+                        lr.getMessage());
+            }
+        };
+        fileHandler.setFormatter(formatter);
+
+        logger.addHandler(fileHandler);
+
+        logger.setLevel(Level.ALL);
+    }
+
+
     // Add a new client thread from the list
     public void addThread(ServerThread st) {
         list.add(st);
+        logger.info("New client thread added. Total clients: " + list.size());
     }
 
     // Remove a client thread from the list
     public void removeThread(ServerThread st) {
         list.remove(st); //remove
+        logger.info("Client thread removed. Total clients: " + list.size());
     }
 
     // Broadcast a message to all connected clients
@@ -55,6 +95,7 @@ public class SocketServer {
         for (ServerThread st : list) {
             st.pw.println(message);
         }
+        logger.info("Broadcasted message: " + message);
     }
 
     public static void main(String[] args) {
@@ -82,6 +123,8 @@ class ServerThread extends Thread {
             pw = new PrintWriter(server.sk.getOutputStream(), true);
             // Read the client's name
             name = br.readLine();
+            // Server logger joined message
+            server.logger.info("New client joined: " + name);
             // Broadcast that a new client has entered
             server.broadCast("**[" + name + "] Entered**");
 
@@ -90,6 +133,7 @@ class ServerThread extends Thread {
                 // Check for list Command
                 if (data == "/list") {
                     pw.println("a");
+                    server.logger.info("Message from " + name + ": " + data);
                 }
                 // Broadcast the received message to all clients
                 server.broadCast("[" + name + "] " + data);
@@ -99,7 +143,9 @@ class ServerThread extends Thread {
             server.removeThread(this);
             // Broadcast that the client has left
             server.broadCast("**[" + name + "] Left**");
+            server.logger.info("Client disconnected: " + name);
             System.out.println(server.sk.getInetAddress() + " - [" + name + "] Exit");
+            server.logger.severe("Error in client thread: " + e.getMessage());
             System.out.println(e + "---->");
         }
     }
