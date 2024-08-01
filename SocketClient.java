5c0a38032679438d8f80384a8f16afc78666a97e
igordon215 <igordon215@gmail.com>
2024-08-01 09:42:44 -0400

    added comments

--- SocketClient.java
+++ SocketClient.java
@@ -9,20 +9,26 @@ import java.net.Socket;
 
 @SuppressWarnings("serial")
 public class SocketClient extends JFrame implements ActionListener, Runnable {
+    // GUI components
     JTextArea textArea = new JTextArea();
     JScrollPane jp = new JScrollPane(textArea);
     JTextField input_Text = new JTextField();
     JMenuBar menuBar = new JMenuBar();
 
+    // Networking components
     Socket sk;
     BufferedReader br;
     PrintWriter pw;
 
+
     public SocketClient() {
         super("Chit Chat");
+        // Set up the main window
         setFont(new Font("Arial Black", Font.PLAIN, 12));
         setForeground(new Color(0, 0, 51));
         setBackground(new Color(51, 0, 0));
+
+        // Configure the text area for chat history
         textArea.setToolTipText("Chat History");
         textArea.setForeground(new Color(50, 205, 50));
         textArea.setEditable(false);
@@ -30,7 +36,7 @@ public class SocketClient extends JFrame implements ActionListener, Runnable {
 
         textArea.setBackground(new Color(0, 0, 0));
 
-/*
+        /*
          JMenu helpMenu = new JMenu("Help");
         JMenuItem update = new JMenuItem("Update Information");
         JMenuItem connect_List = new JMenuItem("Visitor List");
@@ -40,15 +46,21 @@ public class SocketClient extends JFrame implements ActionListener, Runnable {
 
         menuBar.add(helpMenu);
         setJMenuBar(menuBar);
-*/
+        */
+
+        // Add components to the window
         getContentPane().add(jp, "Center");
+
+        // Configure the input text field
         input_Text.setText("Enter your Message:");
         input_Text.setToolTipText("Enter your Message");
         input_Text.setForeground(new Color(0, 0, 0));
         input_Text.setFont(new Font("Tahoma", Font.BOLD, 11));
         input_Text.setBackground(new Color(230, 230, 250));
-        
+
         getContentPane().add(input_Text, "South");
+
+        // Set window properties
         setSize(325, 411);
         setVisible(true);
 
@@ -56,26 +68,31 @@ public class SocketClient extends JFrame implements ActionListener, Runnable {
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // Register action listener for the input field
         input_Text.addActionListener(this); //Event registration
     }
 
     public void serverConnection() {
         try {
+
+            // Prompt for server IP
             String IP = JOptionPane.showInputDialog(this, "Please enter a server IP.", JOptionPane.INFORMATION_MESSAGE);
             sk = new Socket(IP, 1234);
 
+            // Prompt for nickname
             String name = JOptionPane.showInputDialog(this, "Please enter a nickname", JOptionPane.INFORMATION_MESSAGE);
 /*            while (name.length() > 7) {
                 name = JOptionPane.showInputDialog(this, "Please enter a nickname.(7 characters or less)", JOptionPane.INFORMATION_MESSAGE);
             }
 */
-            //read
+            // Set up input and output streams
             br = new BufferedReader(new InputStreamReader(sk.getInputStream()));
 
-            //writing
+            // Send nickname to server
             pw = new PrintWriter(sk.getOutputStream(), true);
             pw.println(name); // Send to server side
 
+            // Start thread to receive messages
             new Thread(this).start();
 
         } catch (Exception e) {
@@ -84,6 +101,7 @@ public class SocketClient extends JFrame implements ActionListener, Runnable {
     }
 
     public static void main(String[] args) {
+        // Create SocketClient instance and connect to server
         new SocketClient().serverConnection(); //Method call at the same time object creation
     }
 
@@ -91,6 +109,7 @@ public class SocketClient extends JFrame implements ActionListener, Runnable {
     public void run() {
         String data = null;
         try {
+            // Continuosly read and display incoming messages
             while ((data = br.readLine()) != null) {
                 textArea.append(data + "\n"); //textArea Decrease the position of the box's scroll bar by the length of the text entered
                 textArea.setCaretPosition(textArea.getText().length());
@@ -102,6 +121,7 @@ public class SocketClient extends JFrame implements ActionListener, Runnable {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        // Send message when Enter is pressed in the input field
         String data = input_Text.getText();
         pw.println(data); // Send to server side
         input_Text.setText("");
--- SocketServer.java
+++ SocketServer.java
@@ -11,41 +11,48 @@ public class SocketServer {
     ServerSocket server;
     Socket sk;
     InetAddress addr;
-    
+
     ArrayList<ServerThread> list = new ArrayList<ServerThread>();
 
     public SocketServer() {
         try {
-        	addr = InetAddress.getByName("127.0.0.1");
-        	//addr = InetAddress.getByName("192.168.43.1");
-            
-        	server = new ServerSocket(1234,50,addr);
+            // Set up the server address (localhost)
+            addr = InetAddress.getByName("127.0.0.1");
+            //addr = InetAddress.getByName("192.168.43.1");
+
+            //Create a ServerSocket on port 1234 with a backlog of 50 connections
+            server = new ServerSocket(1234, 50, addr);
             System.out.println("\n Waiting for Client connection");
+            // Start the client
             SocketClient.main(null);
-            while(true) {
+            while (true) {
+                // Accept incoming client connections
                 sk = server.accept();
                 System.out.println(sk.getInetAddress() + " connect");
 
-                //Thread connected clients to ArrayList
+                //Create and start a new thread for the connected client
                 ServerThread st = new ServerThread(this);
                 addThread(st);
                 st.start();
             }
-        } catch(IOException e) {
+        } catch (IOException e) {
             System.out.println(e + "-> ServerSocket failed");
         }
     }
 
+    // Add a new client thread from the list
     public void addThread(ServerThread st) {
         list.add(st);
     }
 
-    public void removeThread(ServerThread st){
+    // Remove a client thread from the list
+    public void removeThread(ServerThread st) {
         list.remove(st); //remove
     }
 
-    public void broadCast(String message){
-        for(ServerThread st : list){
+    // Broadcast a message to all connected clients
+    public void broadCast(String message) {
+        for (ServerThread st : list) {
             st.pw.println(message);
         }
     }
@@ -55,6 +62,7 @@ public class SocketServer {
     }
 }
 
+//Thread connected clients to ArrayList
 class ServerThread extends Thread {
     SocketServer server;
     PrintWriter pw;
@@ -67,26 +75,31 @@ class ServerThread extends Thread {
     @Override
     public void run() {
         try {
-            // read
+            // Set up reader for incoming messages
             BufferedReader br = new BufferedReader(new InputStreamReader(server.sk.getInputStream()));
 
-            // writing
+            // Set up writer for the outgoing messages
             pw = new PrintWriter(server.sk.getOutputStream(), true);
+            // Read the client's name
             name = br.readLine();
-            server.broadCast("**["+name+"] Entered**");
+            // Broadcast that a new client has entered
+            server.broadCast("**[" + name + "] Entered**");
 
             String data;
-            while((data = br.readLine()) != null ){
-                if(data == "/list"){
+            while ((data = br.readLine()) != null) {
+                // Check for list Command
+                if (data == "/list") {
                     pw.println("a");
                 }
-                server.broadCast("["+name+"] "+ data);
+                // Broadcast the received message to all clients
+                server.broadCast("[" + name + "] " + data);
             }
         } catch (Exception e) {
             //Remove the current thread from the ArrayList.
             server.removeThread(this);
-            server.broadCast("**["+name+"] Left**");
-            System.out.println(server.sk.getInetAddress()+" - ["+name+"] Exit");
+            // Broadcast that the client has left
+            server.broadCast("**[" + name + "] Left**");
+            System.out.println(server.sk.getInetAddress() + " - [" + name + "] Exit");
             System.out.println(e + "---->");
         }
     }
